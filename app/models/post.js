c21f7eea62702834e0df1ce9a8fd239207f7ee8f
isilva <ines.silva@simplabs.com>
2022-04-06 10:02:03 +0100

    Extract the first data of the markdown file

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,7 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class PostModel extends Model {
-  @attr('string') title;
+  @attr title;
+  @attr('date') date;
+  @attr comments;
 }
--- data/post/first.md
+++ data/post/first.md
@@ -1,3 +1,7 @@
 ---
 title: Journey of a Junior Software Engineer
+date: 2018-02-21T00:00:00.000Z
+comments: 3
 ---
+
+hello
