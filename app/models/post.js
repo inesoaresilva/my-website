62f64438377edfdec2b6de0e4811ebd4e473d57e
isilva <ines.silva@simplabs.com>
2022-07-07 10:42:28 +0100

    Add attributes to Post model

--- app/models/post.js
+++ app/models/post.js
@@ -3,5 +3,10 @@ import Model, { attr } from '@ember-data/model';
 export default class PostModel extends Model {
   @attr title;
   @attr('date') date;
+  @attr category;
+  @attr description;
+  @attr image;
   @attr comments;
+  @attr content;
+  @attr html;
 }
