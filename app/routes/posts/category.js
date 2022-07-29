02b4766bb03b961f04feaac395074dd8bae7730b
isilva <ines.silva@simplabs.com>
2022-07-29 14:21:42 +0100

    Show category in action message when filtering the posts

--- app/routes/posts/category.js
+++ app/routes/posts/category.js
@@ -3,8 +3,13 @@ import Route from '@ember/routing/route';
 export default class PostsCategoryRoute extends Route {
   async model(params) {
     const posts = await this.store.findAll('post');
-    return posts.filter((item) => {
+    const filteredPosts = posts.filter((item) => {
       return item.category === params.category;
     });
+
+    return {
+      filteredPosts,
+      category: params.category,
+    };
   }
 }
