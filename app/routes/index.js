e7d21c218f49ed2af07ce592749c3ad06547a31c
isilva <ines.silva@simplabs.com>
2022-09-13 17:52:00 +0100

    Order from the latest to the oldest post

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,9 @@ export default class IndexRoute extends Route {
   async model() {
     const posts = await this.store.findAll('post');
 
-    return posts.sortBy('date').slice(0, 3);
+    return posts
+      .toArray()
+      .sort((a, b) => b.date - a.date)
+      .slice(0, 3);
   }
 }
