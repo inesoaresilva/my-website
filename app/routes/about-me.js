63f5710c7f8f69442b44973de2357665904d0851
isilva <ines.silva@simplabs.com>
2022-09-29 14:29:14 +0100

    Remove model from About me route

--- app/routes/about-me.js
+++ app/routes/about-me.js
@@ -1,7 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class AboutMeRoute extends Route {
-  model() {
-    return ['Junior Software Engineer 👩‍💻', 'Traveller 🌎'];
-  }
-}
+export default class AboutMeRoute extends Route {}
